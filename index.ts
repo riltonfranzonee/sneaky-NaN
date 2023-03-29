fd3b3360e08c66312747b6614d1b3c98932b1ec2
Rilton Franzone <riltonfranzone@gmail.com>
2023-03-29 11:21:07 -0300

    use bitwise operations rather than string manipulation

--- index.ts
+++ index.ts
@@ -4,19 +4,20 @@ const conceal = (message: string): number => {
   const bufferedMessage = Buffer.from(message, "utf-8");
   if (bufferedMessage.length > 6) throw Error("Message can contain at most 6 bytes");
 
-  const signBit = "0";
-  const exponentBits = "1".repeat(11);
+  // Set sign and exponent bits https://en.wikipedia.org/wiki/Double-precision_floating-point_format
+  let ieeeNum: bigint = 0b011111111111n << 52n;
 
-  let num = signBit + exponentBits;
-
-  let significand = "";
-  bufferedMessage.forEach((byte) => {
-    significand += byte.toString(2).padStart(8, "0");
+  bufferedMessage.forEach((byte, idx) => {
+    ieeeNum = ieeeNum | (BigInt(byte) << BigInt(idx * 8));
   });
 
-  num += significand.padStart(52, "0");
-
-  const ieeeBytes = Buffer.from(num.match(/.{1,8}/g)?.map((byte) => parseInt(byte, 2)) || []);
+  const ieeeBytes = Buffer.from(
+    ieeeNum
+      .toString(2)
+      .padStart(64, "0")
+      .match(/.{1,8}/g)
+      ?.map((byte) => parseInt(byte, 2))!
+  );
 
   const unpackedNum = binstruct.def().double().read(ieeeBytes)[0];
 
@@ -26,18 +27,12 @@ const conceal = (message: string): number => {
 const extract = (num: number): string => {
   const floatArray = new Float64Array([num]);
   let bytes = new Uint8Array(floatArray.buffer);
-  const messageBytes = [
-    ...bytes
-      .slice(0, 6)
-      .reverse()
-      .filter((b) => b !== 0),
-  ];
-
+  const messageBytes = [...bytes.slice(0, 6).filter((b) => b !== 0)];
   return Buffer.from(messageBytes).toString("utf-8");
 };
 
-const num = conceal("hi!");
-const decoded = extract(num);
+const encodedNum = conceal("hello!");
+const decodedMessage = extract(encodedNum);
 
-console.log({ num });
-console.log({ decoded });
+console.log({ encodedNum });
+console.log({ decodedMessage });
